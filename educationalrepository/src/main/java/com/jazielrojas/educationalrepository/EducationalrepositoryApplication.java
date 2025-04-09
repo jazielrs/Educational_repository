41e39e2c503dc0719d372d87903ed2b59110040f
jazielrs <jazielrs1@gmail.com>
2025-04-08 19:30:27 -0600

    agregar proyecto base

---
+++ educationalrepository/src/main/java/com/jazielrojas/educationalrepository/EducationalrepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.jazielrojas.educationalrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalrepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationalrepositoryApplication.class, args);
+	}
+
+}
---
+++ educationalrepository/src/test/java/com/jazielrojas/educationalrepository/EducationalrepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jazielrojas.educationalrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalrepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
